c76ea80122b1824bc2b57b89eb3543f58b56a974
min4604 <mina4604sh@gmail.com>
2022-06-03 13:34:34 +0800

    fix seg7

--- seg7/oneseg7/seg7.vhd
+++ seg7/oneseg7/seg7.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity seg7 is
 	port(	bcd : in  std_logic_vector(3 downto 0);
+			dot : in  std_logic;
 			RBI : in  std_logic;
 			RBO : out std_logic;
 			seg : out std_logic_vector(7 downto 0)
@@ -12,17 +13,17 @@ end seg7;
 architecture a of seg7 is
 
 begin
-	seg<= 	"00000011" when (bcd = "0000")and RBI ='1' else
-			"10011111" when bcd="0001" else
-			"00100101" when bcd="0010" else
-			"00001101" when bcd="0011" else
-			"10011001" when bcd="0100" else
-			"01001001" when bcd="0101" else
-			"01000001" when bcd="0110" else
-			"00011111" when bcd="0111" else
-			"00000001" when bcd="1000" else
-			"00001001" when bcd="1001" else
-			"11111111" ;
+	seg<= 	"0000001" & dot when (bcd = "0000")and RBI ='1' else
+			"1001111" & dot when bcd="0001" else
+			"0010010" & dot when bcd="0010" else
+			"0000110" & dot when bcd="0011" else
+			"1001100" & dot when bcd="0100" else
+			"0100100" & dot when bcd="0101" else
+			"0100000" & dot when bcd="0110" else
+			"0001111" & dot when bcd="0111" else
+			"0000000" & dot when bcd="1000" else
+			"0000100" & dot when bcd="1001" else
+			"1111111" & dot ;
 	RBO<= '0' when (RBI='0' and bcd ="0000") else
 		  '1' ;
 end a;
